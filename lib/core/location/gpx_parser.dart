07021f0103fdf95bcf2d1ebf59db1ed96d802792
michroz2 <michroz@gmail.com>
2026-08-30 23:21:42 +0300

    v0.1: Добавлен GpxParser и загрузка маршрута из assets

---
+++ lib/core/location/gpx_parser.dart
@@ -0,0 +1,62 @@
+import 'package:xml/xml.dart';
+import 'package:latlong2/latlong.dart';
+import 'location_entity.dart';
+
+class GpxParser {
+  static List<LocationEntity> parse(String xmlString) {
+    final document = XmlDocument.parse(xmlString);
+    final trkpts = document.findAllElements('trkpt');
+    
+    final List<LocationEntity> locations = [];
+    final distance = const Distance();
+    
+    for (int i = 0; i < trkpts.length; i++) {
+      final trkpt = trkpts.elementAt(i);
+      final latStr = trkpt.getAttribute('lat');
+      final lonStr = trkpt.getAttribute('lon');
+      final eleElement = trkpt.findElements('ele').firstOrNull;
+      final timeElement = trkpt.findElements('time').firstOrNull;
+      
+      if (latStr == null || lonStr == null || eleElement == null || timeElement == null) {
+        continue;
+      }
+      
+      final lat = double.parse(latStr);
+      final lon = double.parse(lonStr);
+      final ele = double.parse(eleElement.innerText);
+      final time = DateTime.parse(timeElement.innerText);
+      
+      double speed = 0.0;
+      double heading = 0.0;
+      
+      if (i > 0) {
+        final prevLocation = locations[i - 1];
+        final prevLatLng = LatLng(prevLocation.latitude, prevLocation.longitude);
+        final currentLatLng = LatLng(lat, lon);
+        
+        final distMeters = distance.as(LengthUnit.Meter, prevLatLng, currentLatLng);
+        final timeDiffSeconds = time.difference(prevLocation.timestamp).inSeconds;
+        
+        if (timeDiffSeconds > 0) {
+          speed = distMeters / timeDiffSeconds;
+        }
+        
+        heading = distance.bearing(prevLatLng, currentLatLng);
+        if (heading < 0) {
+          heading += 360.0;
+        }
+      }
+      
+      locations.add(LocationEntity(
+        latitude: lat,
+        longitude: lon,
+        altitude: ele,
+        speed: speed,
+        heading: heading,
+        timestamp: time,
+      ));
+    }
+    
+    return locations;
+  }
+}
--- lib/core/location/location_state.dart
+++ lib/core/location/location_state.dart
@@ -1,52 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'location_entity.dart';
-import 'location_provider.dart';
 import 'simulator_location_provider.dart';
 
-/// Тестовые данные для симулятора (хардкод из 4 точек полета)
-final List<LocationEntity> _testFlightPoints = [
-  LocationEntity(
-    latitude: 55.751244,
-    longitude: 37.618423,
-    altitude: 150.0,
-    speed: 12.5,
-    heading: 90.0,
-    timestamp: DateTime.now(),
-  ),
-  LocationEntity(
-    latitude: 55.751500,
-    longitude: 37.618900,
-    altitude: 152.0,
-    speed: 13.0,
-    heading: 92.0,
-    timestamp: DateTime.now().add(const Duration(seconds: 2)),
-  ),
-  LocationEntity(
-    latitude: 55.751800,
-    longitude: 37.619500,
-    altitude: 155.0,
-    speed: 13.2,
-    heading: 95.0,
-    timestamp: DateTime.now().add(const Duration(seconds: 5)),
-  ),
-  LocationEntity(
-    latitude: 55.752100,
-    longitude: 37.620200,
-    altitude: 158.0,
-    speed: 13.5,
-    heading: 98.0,
-    timestamp: DateTime.now().add(const Duration(seconds: 7)),
-  ),
-];
+import 'package:flutter/services.dart' show rootBundle;
+import 'gpx_parser.dart';
 
-/// Провайдер, предоставляющий конкретную реализацию интерфейса LocationProvider
-final locationProviderType = Provider<LocationProvider>((ref) {
-  return SimulatorLocationProvider(_testFlightPoints);
+final gpxPointsProvider = FutureProvider<List<LocationEntity>>((ref) async {
+  final xmlString = await rootBundle.loadString('assets/mock_flight.gpx');
+  return GpxParser.parse(xmlString);
 });
 
 /// Провайдер потока данных геолокации, который слушает интерфейс
-final locationStreamProvider = StreamProvider<LocationEntity>((ref) {
-  final locationProvider = ref.watch(locationProviderType);
-  return locationProvider.locationStream;
+final locationStreamProvider = StreamProvider<LocationEntity>((ref) async* {
+  final points = await ref.watch(gpxPointsProvider.future);
+  if (points.isNotEmpty) {
+    final simulator = SimulatorLocationProvider(points);
+    yield* simulator.locationStream;
+  }
 });
